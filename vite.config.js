a9621f6b5f656b856626acfd866ce4c3351d5e85
Nicrayter1 <120420445+Nicrayter1@users.noreply.github.com>
2026-01-08 05:56:55 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bar-stock-app/', // Замените на название вашего репозитория
+  base: '/React-Fullerspub-ast/', // название вашего репозитория
   build: {
     outDir: 'dist',
     sourcemap: false,
