b4fbd261964c1fbbe2b7c39bafc3b865dd053272
Nicrayter1 <120420445+Nicrayter1@users.noreply.github.com>
2026-01-08 05:20:42 +0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'media', // Автоматическая поддержка темной темы
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
