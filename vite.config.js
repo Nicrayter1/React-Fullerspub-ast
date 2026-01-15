f452ebff224971aad1772d509dc917b16f089d11
Nicrayter1 <120420445+Nicrayter1@users.noreply.github.com>
2026-01-16 01:07:44 +0500

    Remove build configuration from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React-Fullerspub-ast/', // название вашего репозитория
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser'
-  }
+  base: '/React-Fullerspub-ast/', // Имя вашего репозитория
 })
