94ced428f6aa6302d76c6cb572c53b61086014df
Nicrayter1 <120420445+Nicrayter1@users.noreply.github.com>
2026-01-20 04:38:47 +0600

    Disable source maps in vite.config.js
    Disable source maps in the build configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/React-Fullerspub-ast/',
   build: {
-    sourcemap: false, // <- Это решит проблему
+    sourcemap: false, // отключаем source maps
+    outDir: 'dist',
   }
 })
