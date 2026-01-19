a13e327fac94e9e386413348095dc9f74b681ce7
Nicrayter1 <120420445+Nicrayter1@users.noreply.github.com>
2026-01-20 04:37:20 +0600

    Update Vite config to include build options
    Added build configuration to disable sourcemaps.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: '/React-Fullerspub-ast/'
+  base: '/React-Fullerspub-ast/',
+  build: {
+    sourcemap: false, // <- Это решит проблему
+  }
 })
